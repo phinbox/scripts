264306fa20fc1d1325546be2a8e5b8ded4cbac01
Phinehas Bynum <phinbox@users.noreply.github.com>
2015-12-18 14:22:49 -0600

    Update update_SCCM_policy.cmd
    Added citation.

--- windows/update_SCCM_policy.cmd
+++ windows/update_SCCM_policy.cmd
@@ -1,3 +1,4 @@
 REM This script triggers the Machine Policy Evaluation and Update Cycle for SCCM connected computers
+REM Sourced from http://blogs.technet.com/b/charlesa_us/archive/2015/03/07/triggering-configmgr-client-actions-with-wmic-without-pesky-right-click-tools.aspx
 
 WMIC /namespace:\\root\ccm path sms_client CALL TriggerSchedule "{00000000-0000-0000-0000-000000000022}" /NOINTERACTIVE
