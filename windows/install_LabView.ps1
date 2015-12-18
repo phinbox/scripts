67c5ce995aa02a9ce99bd76c2861dd942e0b4a4a
Phinehas Bynum <phinbox@users.noreply.github.com>
2015-12-18 14:16:25 -0600

    Update install_LabView.ps1
    Added citation.

--- windows/install_LabView.ps1
+++ windows/install_LabView.ps1
@@ -1,4 +1,5 @@
 #Run as admin if not admin already
+#Sourced from http://stackoverflow.com/questions/7690994/powershell-running-a-command-as-administrator
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
 $arguments = "& '" + $myinvocation.mycommand.definition + "'"
